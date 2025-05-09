5c970bc4d70344ff18b303271ab8c64286559720
Sunil Singh <sksinghaniya5025@gmail.com>
2025-05-09 23:57:52 +0530

    completed a subcription controller by using user model, video model, playlist model beacuse of we are using a channel , channelid, subcription, subcriptionid, and user, userid

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,26 +3,117 @@ import {Like} from "../models/like.model.js"
 import {ApiError} from "../utility(utils)/ApiError.js";
 import {ApiResponse} from "../utility(utils)/ApiResponse.js";
 import {asyncHandler} from "../utility(utils)/async_handler.js";
+import {Video} from "../models/video.model.js";
+import {Tweet} from "../models/tweet.model.js";
+import {Comment} from "../models/comment.model.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid video id")
+    }
+    const video = await Video.findById(videoId)
+    if(!video){
+        throw new ApiError(404, "Video not found")
+    }
+    const userId = req.user.id // Assuming userId is available in req.user
+    const like = await Like.findOne({video: videoId, user: userId})
+    if(!like){
+        // If like doesn't exist, create a new like
+        const newLike = await Like.create({video: videoId, user: userId})
+        return res.status(201).json(
+            new ApiResponse(true, 201, "Video liked successfully", newLike)
+        )
+    }
+    // If like exists, remove it
+    const removedLike = await Like.findByIdAndDelete(like._id)
+    if(!removedLike){
+        throw new ApiError(404, "Like not found")
+    }
+    return res.status(200)
+    .json(
+        new ApiResponse(true, 200, "Video unliked successfully", removedLike)
+    )
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
+    if(!isValidObjectId(commentId)){
+        throw new ApiError(400, "Invalid comment id")
+    }
+    const comment =  await Like.findById(commentId)
+    if(!comment){
+        throw new ApiError(404, "Comment not found")
+    }
+    const userId = req.user.id // Assuming userId is available in req.user
+    const like = await Like.findOne({comment: commentId, user: userId})
+    if(!like){
+        // If like doesn't exist, create a new like
+        const newLike = await Like.create({comment: commentId, user: userId})
+        return res.status(201).json(
+            new ApiResponse(true, 201, "Comment liked successfully", newLike)
+        )
+    }
+    // If like exists, remove it
+    const likeRemoved =  await Like.findByIdAndDelete(like._id)
+   
+    return res.status(200)
+    .json(
+        new ApiResponse(true, 200, "Comment unliked successfully", likeRemoved)
+    )
 
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400, "Invalid tweet id")
+    }
+    const tweet = await Like.findById(tweetId)
+    if(!tweet){
+        throw new ApiError(404, "Tweet not found")
+    }
+    const userId = req.user.id // Assuming userId is available in req.user
+    const like= await Like.findOne({tweet: tweetId, user: userId})
+    if(!like){
+        // If like doesn't exist, create a new like
+        const newLike = await Like.create({tweet: tweetId, user: userId})
+        return res.status(201).json(
+            new ApiResponse(true, 201, "Tweet liked successfully", newLike)
+        )
+    }
+    // If like exists, remove it
+    const deleteLike = await Like.findByIdAndDelete(like._id)
+    return res.status(200)
+    .json(
+        new ApiResponse(true, 200, "Tweet unliked successfully", deleteLike)
+    )
 }
 )
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+    const userId = req.user.id // Assuming userId is available in req.user
+    const likedVideos = await Like.find({user: userId}).populate("video")
+    if(likedVideos.length === 0){
+        throw new ApiError(404, "No Liked videos found")
+    }
+  const likedVideosList = likedVideos.map((video) => {
+        return {
+            id: video._id,
+            title: video.title,
+            category: video.category,
+            isPublished: video.isPublished,
+            createdAt: video.createdAt
+        }
+  })  
+  return res.status(200)
+  .json(
+        new ApiResponse(true, 200, "Liked videos fetched successfully", likedVideosList)
+  )
 })
 
 export {
--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,6 +1,8 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {User} from "../models/user.model.js"
-import { Subscription } from "../models/subscription.model.js"
+import mongoose, {isValidObjectId} from "mongoose";
+import {User} from "../models/user.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import {Video} from "../models/video.model.js";
+import {Playlist} from "../models/playlist.model.js";
 import {ApiError} from "../utility(utils)/ApiError.js";
 import {ApiResponse} from "../utility(utils)/ApiResponse.js";
 import {asyncHandler} from "../utility(utils)/async_handler.js";
@@ -9,16 +11,102 @@ import {asyncHandler} from "../utility(utils)/async_handler.js";
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400, "Invalid channel id")
+    }
+    const channel = await User.findById(channelId)
+    if(!channel){
+        throw new ApiError(404, "Channel not found")
+    }
+    const userId = req.user.id // Assuming userId is available in req.user
+    const subscription = await Subscription.findOne({channel: channelId, user: userId})
+    if(!subscription){
+        // if subscription doesnt exist, create a new subscription
+        const newSubscription = await Subscription.create({channel: channelId, user: userId})
+        return res.status(201)
+        .json(
+            new ApiResponse(true, 201, "Subscribed to channel successfully", newSubscription)
+        )
+    }
+    // if subscription exists, remove it
+    const removedSubscription = await Subscription.findByIdAndDelete(subscription._id)
+    return res.status(200)
+    .json(
+        new ApiResponse(true, 200, "Unsubscribed from channel successfully", removedSubscription)
+    )
 })
+
+
+
+
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400, "Invalid channel id")
+    }
+    const channel = await User.findById(channelId)
+    if(!channel){
+        throw new ApiError(404, "Channel not found")
+    }
+    const subscribers = await Subscription.find({channel: channelId});
+    if(subscribers.length === 0){
+        throw new ApiError(404, "No subscribers found for this channel")
+    }
+    const subscriberList = subscribers.map((subscriber) => {
+        return {
+            id: subscriber._id,
+            userId: subscriber.user,
+            channelId: subscriber.channel,
+            createdAt: subscriber.createdAt,
+            updatedAt: subscriber.updatedAt
+        }
+    })
+    return res.status(200)
+    .json(
+        new ApiResponse(true, 200, "Subscribers fetched successfully", subscriberList)
+    )
 })
+
+
+
+
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+    if(!isValidObjectId(subscriberId)){
+        throw new ApiError(400, "Invalid subscriber id")
+    }
+    const subscriber = await User.findById(subscriberId)
+    if(!subscriber){
+        throw new ApiError(404, "Subscriber not found")
+    }
+    const subscriptions = await Subscription.find({user: subscriberId}).populate("channel")
+    if(!subscriptions || subscriptions.length === 0){
+        throw new ApiError(404, "No subscriptions found for this user")
+    }
+    const subscribedChannel = subscriptions.map((subscription) => {
+        return {
+            id: subscription._id,
+            channelId: subscription.channel._id,
+            channelName: subscription.channel.name,
+            channelDescription: subscription.channel.description,
+            channelThumbnail: subscription.channel.thumbnail,
+            channelCreatedAt: subscription.channel.createdAt,
+            channelUpdatedAt: subscription.channel.updatedAt,
+            subscriberId: subscription.user,
+            createdAt: subscription.createdAt,
+            updatedAt: subscription.updatedAt
+
+        }
+    })
+    return res.status(200)
+    .json(
+        new ApiResponse(true, 200, "Subscribed channels fetched successfully", subscribedChannel)
+    )
+
 })
 
 export {
